7c5737e190d1cf07f7cb216118dd20fb5d904f3b
Federico Malesani <f.malesani@gmail.com>
2015-09-03 16:32:50 +1000

    user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903061146) do
+ActiveRecord::Schema.define(version: 20150903063130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20150903061146) do
     t.string   "accuracy"
     t.string   "speed"
     t.string   "bearing"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "surname"
+    t.string   "email"
+    t.string   "password"
+    t.string   "bithday"
+    t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
