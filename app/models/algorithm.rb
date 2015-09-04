b95fb4464be1110c589607d89e78fee773a0d5a9
Federico Malesani <f.malesani@gmail.com>
2015-09-04 11:55:46 +1000

    no message

--- app/models/algorithm.rb
+++ app/models/algorithm.rb
@@ -21,37 +21,37 @@ class Algorithm < ActiveRecord::Base
     	e2.trip_id = t.id
     	e2.save
 
-		tf1 = TransferZone.new
-		# tf1.time = nil
-    	tf1.latitude = "111"
-    	tf1.longitude = "222"
-    	tf1.altitude = "333"
-    	tf1.event_id = e1.id
-    	tf1.save
+		# tf1 = TransferZone.new
+		# # tf1.time = nil
+  #   	tf1.latitude = "111"
+  #   	tf1.longitude = "222"
+  #   	tf1.altitude = "333"
+  #   	tf1.event_id = e1.id
+  #   	tf1.save
 
-    	tf2 = TransferZone.new
-		# tf2.time = nil
-    	tf2.latitude = "1111"
-    	tf2.longitude = "2222"
-    	tf2.altitude = "3333"
-    	tf2.event_id = e1.id
-    	tf2.save
+  #   	tf2 = TransferZone.new
+		# # tf2.time = nil
+  #   	tf2.latitude = "1111"
+  #   	tf2.longitude = "2222"
+  #   	tf2.altitude = "3333"
+  #   	tf2.event_id = e1.id
+  #   	tf2.save
 
-    	tf3 = TransferZone.new
-		# tf3.time = nil
-    	tf3.latitude = "1111"
-    	tf3.longitude = "2222"
-    	tf3.altitude = "3333"
-    	tf3.event_id = e2.id
-    	tf3.save
+  #   	tf3 = TransferZone.new
+		# # tf3.time = nil
+  #   	tf3.latitude = "1111"
+  #   	tf3.longitude = "2222"
+  #   	tf3.altitude = "3333"
+  #   	tf3.event_id = e2.id
+  #   	tf3.save
 
-    	tf4 = TransferZone.new
-		# tf4.time = nil
-    	tf4.latitude = "111"
-    	tf4.longitude = "222"
-    	tf4.altitude = "333"
-    	tf4.event_id = e2.id
-    	tf4.save
+  #   	tf4 = TransferZone.new
+		# # tf4.time = nil
+  #   	tf4.latitude = "111"
+  #   	tf4.longitude = "222"
+  #   	tf4.altitude = "333"
+  #   	tf4.event_id = e2.id
+  #   	tf4.save
     	
 	end
 
