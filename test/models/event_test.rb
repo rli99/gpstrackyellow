75e5eaf5c610d184dc366ac413f04c0395f06c02
Federico Malesani <f.malesani@gmail.com>
2015-09-04 12:15:46 +1000

    no message

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -4,4 +4,11 @@ class EventTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+	test "the truth" do
+		e = Event.find(1)
+
+  		assert (e.transportation == "bus edited" || e.transportation == "car edited")
+  	end
 end
--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -4,4 +4,6 @@ class TripTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
 end
+
