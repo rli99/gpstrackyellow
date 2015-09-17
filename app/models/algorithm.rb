d11523b97957f24cc8cba9ad2dae254693565670
Federico Malesani <f.malesani@gmail.com>
2015-09-17 19:30:43 +1000

    awegwe

--- app/models/algorithm.rb
+++ app/models/algorithm.rb
@@ -13,7 +13,7 @@ class Algorithm < ActiveRecord::Base
     e.trip_id = t.id
     e.save
     p1 = gpsData[0]
-    p2 = gpsData[5]
+    p2 = gpsData[-1]
 
     t1.latitude = p1.latitude
     t1.longitude = p1.longitude
@@ -27,34 +27,7 @@ class Algorithm < ActiveRecord::Base
     t2.event_id = e.id
     t2.save 
 
-    count = 0
     gpsData.each do |point|
-      if count == 5
-        # count = 0
-        e = Event.new
-        t3 = TransferZone.new
-        t4 = TransferZone.new
-        e.transportation = "bus"
-        e.trip_id = t.id
-        e.save
-        p3 = gpsData[5]
-        p4 = gpsData[10]
-
-        t3.latitude = p3.latitude
-        t3.longitude = p3.longitude
-        t3.time = p3.time
-        t3.event_id = e.id
-        t3.save
-
-        t4.latitude = p4.latitude
-        t4.longitude = p4.longitude
-        t4.time = p4.time
-        t4.event_id = e.id
-        t4.save 
-
-      end
-
-      count += 1
       i = Intermediatepoint.new
       i.latitude = point.latitude
       i.longitude = point.longitude
