f92e4a0786df9080cc6fe28e330492ecca12d452
Roger Li <rogerli9999@hotmail.com>
2015-09-17 16:25:41 +1000

    test

--- app/models/algorithm.rb
+++ app/models/algorithm.rb
@@ -2,38 +2,39 @@ class Algorithm < ActiveRecord::Base
 
 	def self.transform (gpsData)
 
-      t = Trip.new
-  t1 = TransferZone.new
-  t2 = TransferZone.new
-  e = Event.new
-  p1 = gpsData[0]
-  p2 = gpsData[-1]
-  t.save
+    t = Trip.new
+    t.avgSpeed = "10km/hr"
+    t.duration = "2hrs"
+    t.save
+    t1 = TransferZone.new
+    t2 = TransferZone.new
+    e = Event.new
+    e.transportation = "walking"
+    e.save
+    p1 = gpsData[0]
+    p2 = gpsData[-1]
 
-  t1.latitude = p1.latitude
-  t1.longitude = p1.longitude
-  t1.time = p1.time
-  t1.event_id = e.id
-  t1.save
+    t1.latitude = p1.latitude
+    t1.longitude = p1.longitude
+    t1.time = p1.time
+    t1.event_id = e.id
+    t1.save
 
-  t2.latitude = p2.latitude
-  t2.longitude = p2.longitude
-  t2.time = p2.time
-  t2.event_id = e.id
-  t2.save 
-  
-  e.save
+    t2.latitude = p2.latitude
+    t2.longitude = p2.longitude
+    t2.time = p2.time
+    t2.event_id = e.id
+    t2.save 
 
-  gpsData.each do |point|
-    i = Intermediatepoint.new
-    i.latitude = point.latitude
-    i.longitude = point.longitude
-    i.time = point.time
-    i.trip_id = t.id
-    i.save
-  end
+    gpsData.each do |point|
+      i = Intermediatepoint.new
+      i.latitude = point.latitude
+      i.longitude = point.longitude
+      i.time = point.time
+      i.trip_id = t.id
+      i.save
+    end
 
-  t.save
 
 		# puts "@@@@@@@@@@"
 		# puts gpsdata_block
