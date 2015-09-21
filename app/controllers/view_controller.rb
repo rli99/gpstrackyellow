0a1e243a97709f41101156592eb7af0d2eb6dd58
Roger Li <rogerli9999@hotmail.com>
2015-09-21 18:55:21 +1000

    typo

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -13,8 +13,8 @@ class ViewController < ApplicationController
 
 	def gmap
 		
-		if Trip.find_by(id:10)
-			events = Trip.find_by(id:10).events
+		if Trip.find_by(id:11)
+			events = Trip.find_by(id:11).events
 
 			@hash_eventsData = []
 
