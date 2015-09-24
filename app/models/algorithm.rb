8a8ee51196c011d7b2752a925aa118e70007596c
Roger Li <rogerli9999@hotmail.com>
2015-09-24 19:24:30 +1000

    wefwef

--- app/models/algorithm.rb
+++ app/models/algorithm.rb
@@ -84,7 +84,7 @@ class Algorithm < ActiveRecord::Base
 
         end
         t.avgSpeed = ((3.6 * totalAvgSpeed) / gpsData.length).round(2).to_s + "km/h"
-        t.distance = (((3.6 * totalAvgSpeed) / gpsData.length) * t.duration).round(2).to_s + "km"
+        # t.distance = (((3.6 * totalAvgSpeed) / gpsData.length) * t.duration.to_f).round(2).to_s + "km"
         t.verified = "false"
         t.save
     end
