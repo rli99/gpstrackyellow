14a4d7a7359d3ec37733d5637f16d1df09737698
Roger Li <rogerli9999@hotmail.com>
2015-09-24 15:20:13 +1000

    fix

--- app/assets/javascripts/drawTripIntermediatePoints.js
+++ app/assets/javascripts/drawTripIntermediatePoints.js
@@ -1,6 +1,6 @@
 function drawTripIntermediatePoints(eventsData) {
     for(var i=0;i<eventsData.length;i++){
-      var intPoints = eventsData[i]["intermediatepoints"].slice(1, eventsData[i]["intermediatepoints"].length);
+      var intPoints = (eventsData[i]["intermediatepoints"]).slice(1, [i]["intermediatepoints"].length - 1);
       intPoints.sort(compareTime);
       for(var j=0;j<intPoints.length;j++){
         makeIntPoint(intPoints[j]);        
