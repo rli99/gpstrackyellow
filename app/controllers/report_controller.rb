66e75a4424a6eb622acf94bf2338c855beb09bf8
rli99 <rogerli99999@gmail.com>
2015-10-15 12:00:44 +0000

    Tables for report page.

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -22,9 +22,16 @@ class ReportController < ApplicationController
      @walking = 0
      @tram = 0
      @car = 0
-     @walkingtotalespeed = 0
-     @tramtotalespeed = 0
-     @cartotalespeed = 0
+     @walking_events = 0
+     @tram_events = 0
+     @car_events = 0
+     @walkingtotalespeed = 0.0
+     @tramtotalespeed = 0.0
+     @cartotalespeed = 0.0
+     @walkingtotalduration = 0.0
+     @tramtotalduration = 0.0
+     @cartotalduration = 0.0
+     
      
        puts "----------aaaaaaaaa---------------"
         Trip.all.each do |t|
@@ -47,42 +54,51 @@ class ReportController < ApplicationController
 	    end 
 	    
 	    @eventresult.each do |t|
-	        if t.transportation = "walking"
-	            @walking += 1
-	            @walkingtotalduration = ((t.intermediatepoints[-1].time - t.intermediatepoints[0].time)/3600).to_f
+	        if t.transportation == "walking"
+	        	@walking_events += 1
+	            @walkingtotalduration += ((t.intermediatepoints[0].time - t.intermediatepoints[-1].time)/60).round(2).to_f
 	             t.intermediatepoints.each do |e|
-	                 @walkingtotalespeed += e.speed.at(0..-5).to_f
+	             	@walking += 1
+	                 @walkingtotalespeed += e.speed.to_f * 3.6
 	             end
-	             @walkingavgduration = @walkingtotalduration/@walking
-	             @walkingavgspeed = @walkingtotalespeed/@walking
-	             @walkingtotaldistance = @walkingavgspeed*@walkingtotalduration
-	             @walkingavgdistance = @walkingtotaldistance/@walking
+	             
 	        end 
 	        
-	        if t.transportation = "car"
-	            @car += 1
-	            @cartotalduration = ((t.intermediatepoints[-1].time - t.intermediatepoints[0].time)/3600).to_f
+	        if t.transportation == "car"
+	        	@car_events += 1
+	            @cartotalduration += ((t.intermediatepoints[0].time - t.intermediatepoints[-1].time)/60).round(2).to_f
 	             t.intermediatepoints.each do |e|
-	                 @cartotalespeed += e.speed.at(0..-5).to_f
+	             	@car += 1
+	                 @cartotalespeed += e.speed.to_f * 3.6
 	             end
-	             @caravgduration = @cartotalduration/@car
-	             @caravgspeed = @cartotalespeed/@car
-	             @cartotaldistance = @caravgspeed*@cartotalduration
-	             @caravgdistance = @cartotaldistance/@car
+
 	        end 
 	        
-	        if t.transportation = "tram"
-	            @tram += 1
-	            @tramtotalduration = ((t.intermediatepoints[-1].time - t.intermediatepoints[0].time)/3600).to_f
+	        if t.transportation == "tram"
+	        	@tram_events += 1
+	            @tramtotalduration += ((t.intermediatepoints[0].time - t.intermediatepoints[-1].time)/60).round(2).to_f
 	             t.intermediatepoints.each do |e|
-	                 @tramtotalespeed += e.speed.at(0..-5).to_f
+	             	@tram += 1
+	                 @tramtotalespeed += e.speed.to_f * 3.6
 	             end
-	             @tramavgduration = @tramtotalduration/@tram
-	             @tramavgspeed = @tramtotalespeed/@tram
-	             @tramtotaldistance = @tramavgspeed*@tramtotalduration
-	             @tramavgdistance = @tramtotaldistance/@tram
 	        end 
 	    end 
+
+        @walkingavgduration = (@walkingtotalduration/@walking_events).round(2)
+         @walkingavgspeed = (@walkingtotalespeed/@walking).round(2)
+         @walkingtotaldistance = (@walkingavgspeed*@walkingtotalduration / 60).round(2)
+         @walkingavgdistance = (@walkingtotaldistance/@walking_events).round(2)
+
+         @caravgduration = (@cartotalduration/@car_events).round(2)
+         @caravgspeed = (@cartotalespeed/@car).round(2)
+         @cartotaldistance = (@caravgspeed*@cartotalduration / 60).round(2)
+         @caravgdistance = (@cartotaldistance/@car_events).round(2)
+	   
+
+         @tramavgduration = (@tramtotalduration/@tram_events).round(2)
+         @tramavgspeed = (@tramtotalespeed/@tram).round(2)
+         @tramtotaldistance = (@tramavgspeed*@tramtotalduration / 60).round(2)
+         @tramavgdistance = (@tramtotaldistance/@tram_events).round(2)
 	   
       #number of trips  
      @tripresultcount = @tripresult.count()
@@ -98,14 +114,14 @@ class ReportController < ApplicationController
      end 
      
      #total distance, duration, avgspeed of trips
-     @tripresultsumdistance = sum(arrtripdistance)
-     @tripresultsumduration = sum(arrtripduration)
-     @tripresultsumavgSpeed = sum(arrtripavgSpeed)
+     @tripresultsumdistance = sum(arrtripdistance).round(2)
+     @tripresultsumduration = sum(arrtripduration).round(2)
+     @tripresultsumavgSpeed = sum(arrtripavgSpeed).round(2)
        
      # average of distance, duration, avgspeed of trips
-     @avetripresultdistance = @tripresultsumdistance/@tripresultcount
-     @avetripresultduration = @tripresultsumduration/@tripresultcount
-     @avetripresultavgSpeed = @tripresultsumavgSpeed/@tripresultcount
+     @avetripresultdistance = (@tripresultsumdistance/@tripresultcount).round(2)
+     @avetripresultduration = (@tripresultsumduration/@tripresultcount).round(2)
+     @avetripresultavgSpeed = (@tripresultsumavgSpeed/@tripresultcount).round(2)
 
     end
     
