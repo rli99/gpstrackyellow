f6e7e14b6e5245fccb546ac4cdeb49e949dbb9f2
rli99 <rogerli99999@gmail.com>
2015-10-18 04:41:47 +0000

    Instructions for users/researchers.

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,8 @@ Rails.application.routes.draw do
   post '/view/tripdata/filter' => 'view#tripdata'
   delete '/view/tripdata' => 'trip#destroy'
   post '/report/show' => 'report#show'
+  get 'welcome/user_instruction'
+  get 'welcome/researcher_instruction'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
